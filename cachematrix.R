cb138f0edaeff0e64360cbf510a49271b0870ff4
sasi-patha <sasi.patha@gmail.com>
2016-11-12 21:05:12 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,77 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
+## Function makeCacheMatrix creates variables and functions(getters, setters) for Matrix, its Inverse. 
+## Function cacheSolve is called for setting a new Matrix as well, getting inverse from cache 
+## if already present or Calculate the inverse of a new matrix and return it.
 
-## Write a short comment describing this function
+## Write a short comment describing this function - makeCacheMatrix
+## Function makeCacheMatrix should first be supplied by a square Matrix for which inverse exists.
+## this function returns the Function objects of - getters, setters as a list. The GLobal 
+## environment would contain the variables(matrix, inverse matrix, ) and functions.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(M = matrix()) 
+{
+  
+  I <- NULL         #inverse of the input Matrix
+  prevM <- NULL     # previous matrix for which inverse was calculated.
+  
+  # set, get Functions on input Matrix
+  setmatrix         <- function(m) {
+    M <<- m                                                 
+  }
+  getmatrix         <- function() M
+  
+  # set, get Functions on previous Matrix for which Inverse was calculated.
+  cachematrix       <- function(prevm) {
+    prevM <<- prevm
+  }
+  getcachematrix    <- function() prevM
+  
+  # set, get Functions on Inverse of the input Matrix
+  cacheinverse      <- function(inv) I <<- inv
+  getinverse        <- function() I
+  
+  # Return the list of Functions   
+  list( setmat     = setmatrix,    getmat     = getmatrix,
+        setprevmat = cachematrix,  getprevmat = getcachematrix,
+        setinv     = cacheinverse, getinv     = getinverse )
 }
 
 
 ## Write a short comment describing this function
+## Function cacheSolve is called to calculate the Inverse of a Matrix if it is not cached already.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mCM = makeCacheMatrix(), ...) 
+  
+{
+  
+  #Assumption : For this assignment, the matrix supplied is always invertible( Square matrix and determinant <> 0)
+  # Return the inverse of x, or calculate & return if cache is empty
+  
+  i     <- mCM$getinv()
+  prevm <- mCM$getprevmat()
+  m     <- mCM$getmat()
+  
+  if(identical(m,prevm) & !is.null(i) ) {
+    
+    message("Getting cached date for inverse of Matrix:")
+    return(i)
+  } 
+  else { 
+    print("here2")
+    i <- solve(m) 
+    mCM$setinv(i)
+    mCM$setprevmat(m)
+    print("Calculated inverse of the newMatrix:")
+    return(i)
+  }
+  
 }
+
+
+
+
+
+
+
+
+
